9d728590e2558edccb2016446accdba98f79a7c7
Thimo <thimoderam@gmail.com>
2023-05-07 12:15:02 +0200

    [Added] project

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+'use client'
+import {RunSSR} from '@/app/Examples.js'
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+     <RunSSR />
+    </main>
+  )
+}
